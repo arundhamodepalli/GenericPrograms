01288dcff2790695af35a6aa8327642394fe8ae5
ammu72642668 <arundhamodepalli726426@gmail.com>
2023-03-13 19:35:16 +0530

    maximum integer in generic programs

---
+++ GenericsPrograms/MaximumInteger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsPrograms
+{
+    internal class MaximumInteger
+    {
+          //Return Maximum values out of three
+            public static int MaxNumCheck(int firstValue, int secondValue, int thirdValue)
+            {
+
+                if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+                {
+                    return firstValue;
+                }
+                if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+                {
+                    return secondValue;
+                }
+                if (thirdValue.CompareTo(secondValue) > 0 && thirdValue.CompareTo(firstValue) > 0)
+                {
+                    return thirdValue;
+                }
+                throw new Exception("FirstNumber,Second Number , Third Number are same");
+            }
+        }
+    }
--- GenericsPrograms/Program.cs
+++ GenericsPrograms/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("welcome to generic programs");
+            int result;
+            //To find Greatest among 3 number
+            //T.C1 Max at first Position
+            result = MaximumInteger.MaxNumCheck(4, 3, 2);
+            Console.WriteLine("The greater number is " + result);
+            //T.C2 Max at second Position
+            result = MaximumInteger.MaxNumCheck(2, 5, 3);
+            Console.WriteLine("The greater number is " + result);
+            //T.C3 Max at third Positon
+            result = MaximumInteger.MaxNumCheck(2, 6, 8);
+            Console.WriteLine("The greater number is " + result);
+
         }
     }
 }
