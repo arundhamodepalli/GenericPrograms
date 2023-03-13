9c2c811263515cff8013a0cde667074ea623606d
ammu72642668 <arundhamodepalli726426@gmail.com>
2023-03-13 19:44:10 +0530

    maximum Float in generic programs

--- GenericsPrograms/MaximumInteger.cs
+++ GenericsPrograms/MaximumInteger.cs
@@ -8,23 +8,43 @@ namespace GenericsPrograms
 {
     internal class MaximumInteger
     {
-          //Return Maximum values out of three
-            public static int MaxNumCheck(int firstValue, int secondValue, int thirdValue)
-            {
+        //Return Maximum values out of three
+        public static int MaxNumCheck(int firstValue, int secondValue, int thirdValue)
+        {
 
-                if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
-                {
-                    return firstValue;
-                }
-                if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
-                {
-                    return secondValue;
-                }
-                if (thirdValue.CompareTo(secondValue) > 0 && thirdValue.CompareTo(firstValue) > 0)
-                {
-                    return thirdValue;
-                }
-                throw new Exception("FirstNumber,Second Number , Third Number are same");
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
             }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(secondValue) > 0 && thirdValue.CompareTo(firstValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("FirstNumber,Second Number , Third Number are same");
+
+
+        }
+
+        public static double MaximumFloatCheck(double firstValue, double secondValue, double thirdValue)
+        {
+
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(secondValue) > 0 && thirdValue.CompareTo(firstValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("FirstNumber,Second Number , Third Number are same");
+        }
         }
     }
--- GenericsPrograms/Program.cs
+++ GenericsPrograms/Program.cs
@@ -4,17 +4,33 @@
     {
         static void Main(string[] args)
         {
-            int result;
+            int maxInt;
+            double maxFloat;
             //To find Greatest among 3 number
             //T.C1 Max at first Position
-            result = MaximumInteger.MaxNumCheck(4, 3, 2);
-            Console.WriteLine("The greater number is " + result);
+            maxInt = MaximumInteger.MaxNumCheck(4, 3, 2);
+            Console.WriteLine("The greater number among 4,3,2 is " + maxInt);
             //T.C2 Max at second Position
-            result = MaximumInteger.MaxNumCheck(2, 5, 3);
-            Console.WriteLine("The greater number is " + result);
+            maxInt = MaximumInteger.MaxNumCheck(2, 5, 3);
+            Console.WriteLine("The greater number among 2, 5, 3 is " + maxInt);
             //T.C3 Max at third Positon
-            result = MaximumInteger.MaxNumCheck(2, 6, 8);
-            Console.WriteLine("The greater number is " + result);
+            maxInt = MaximumInteger.MaxNumCheck(2, 6, 8);
+            Console.WriteLine("The greater number among 2, 6, 8 is " + maxInt);
+
+            Console.WriteLine("The greatest among float values");
+            //Maximum among three float Integers
+            //T.C1 Max at first Position
+            maxFloat = MaximumInteger.MaximumFloatCheck(4.4, 2.2, 3.3);
+            Console.WriteLine("The greater Float among 4.4,2.2,3.3 is " + maxFloat);
+            //T.C2 Max at second Position
+            maxFloat = MaximumInteger.MaximumFloatCheck(4.4, 5.5, 3.3);
+            Console.WriteLine("The greater Float among 4.4, 5.5, 3.3 is " + maxFloat);
+            //T.C3 Max at third Position
+            maxFloat = MaximumInteger.MaximumFloatCheck(4.4, 2.2, 6.6);
+            Console.WriteLine("The greater Float among 4.4, 2.2, 6.6 is " + maxFloat);
+
+
+
 
         }
     }
